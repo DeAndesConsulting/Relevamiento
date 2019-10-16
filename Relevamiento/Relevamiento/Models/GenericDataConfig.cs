9ad5006cee2d2ddcda4443d9c215726a00105a07
Fernando Schlottmann <fgc.schlottmann@gmail.com>
2019-10-16 02:18:09 -0300

    falto un archivo para que funcione lo del push anterior

---
+++ Relevamiento/Relevamiento/Models/GenericDataConfig.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Relevamiento.Models
+{
+    public class GenericDataConfig
+    {
+        [PrimaryKey]
+        public int ID { get; set; }
+        public bool isSynchronized { get; set; }
+        public DateTime lastSynchronized { get; set; }
+    }
+}
